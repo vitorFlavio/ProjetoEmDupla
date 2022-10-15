e9bc90d075c9c044877bf023438a2538524f238a
vitorFlavio <vitorflavio14@gmail.com>
2022-10-15 12:37:30 -0300

    (EmAndamento)

--- ProjetoEmDupla.c
+++ ProjetoEmDupla.c
@@ -51,7 +51,7 @@ int main()
                 printf("=============================================================================================================\n");
                 scanf("%d", &tipo);
 
-                 float resultadoD=0;
+                float resultadoD = 0;
 
                 switch (tipo)
                 {
@@ -97,21 +97,19 @@ int main()
                     break;
 
                 case 4:
-                    
-                   
 
                     printf("__________________________TABUADA DA DIVISÃO INTEIRA DO %d________________________________\n", tabuada);
                     for (i = valorTabuada; i >= 0; i--)
                     {
 
-                       resultadoD = (float)i/(float)tabuada;
-                    
-                       if(i%tabuada==0){
-                        printf("|");
-                        printf(" %d : %d = %.0f", i, tabuada, resultadoD);
-                        printf("|\n");
-                       }
-                        
+                        resultadoD = (float)i / (float)tabuada;
+
+                        if (i % tabuada == 0)
+                        {
+                            printf("|");
+                            printf(" %d : %d = %.0f", i, tabuada, resultadoD);
+                            printf("|\n");
+                        }
                     }
                     break;
 
@@ -120,7 +118,9 @@ int main()
                 }
 
                 printf("\n\n");
+                 printf("...........................................................................................................................\n");
                 printf("%s deseja informar uma nova tabuada?:\nPra sim digite 1.\nPra sair digite qualquer outro valor\n", nome);
+                 printf("...........................................................................................................................\n");
                 scanf("%d", &opcao2);
                 system("clear");
 
@@ -133,172 +133,159 @@ int main()
         else if (opcao == 2)
         {
 
-            while(opcao2==1){
-            // PARTE DA CALCULADORA:
-            printf("#####################################################################################################\n");
-            //variaveis calculaddora
-            int tipo2, i, resto = 0, valor1r = 0, valor2r = 0, resR = 0;
-            float valor1, valor2, resultadoCal = 0;
-            int pot = 1, valor1p, valor2p;
-
-            printf("Escolha o tipo de operação:\n1.Soma\n2.Subtração\n3.Multiplicação\n4.Divisão\n5.Potenciação\n");
-            printf("#####################################################################################################\n");
-            scanf("%d", &tipo2);
-
-            switch (tipo2)
+            while (opcao2 == 1)
             {
+                // PARTE DA CALCULADORA:
+                printf("#####################################################################################################\n");
+                // variaveis calculaddora
+                int tipo2, i, resto = 0, valor1r = 0, valor2r = 0, resR = 0;
+                float valor1, valor2, resultadoCal = 0;
+                int pot = 1, valor1p, valor2p;
 
-            case 1:
+                printf("Escolha o tipo de operação:\n1.Soma\n2.Subtração\n3.Multiplicação\n4.Divisão\n5.Potenciação\n");
+                printf("#####################################################################################################\n");
+                scanf("%d", &tipo2);
 
-                // INterface do usuario da calculadora
-                printf("SOMA:\n");
-                printf("-->");
-                scanf("%f", &valor1);
-                printf("mais\n");
-                printf("-->");
-                scanf("%f", &valor2);
-                printf("\n");
-
-                // realiza a operação de soma
-                resultadoCal = valor1 + valor2;
-
-                printf(">>> %.2f + %.2f = %.2f\n", valor1, valor2, resultadoCal);
-
-                break;
-
-            case 2:
-
-                printf("SUBTRAÇÃO:\n");
-                printf("-->");
-                scanf("%f", &valor1);
-                printf("menos\n");
-                printf("-->");
-                scanf("%f", &valor2);
-                printf("\n");
-
-                // Realiza a operação de subtração
-                resultadoCal = valor1 - valor2;
-
-                printf(">>> %.2f - %.2f = %.2f\n", valor1, valor2, resultadoCal);
-                break;
-
-            case 3:
-
-                printf("MULTIPLICAÇÃO:\n");
-                printf("-->");
-                scanf("%f", &valor1);
-                printf("vezes\n");
-                printf("-->");
-                scanf("%f", &valor2);
-                printf("\n");
-
-                // Realiza a operação de multiplicação
-                resultadoCal = valor1 * valor2;
-
-                printf(">>> %.2f x %.2f = %.2f\n", valor1, valor2, resultadoCal);
-                printf("\n");
-                printf("#########################################################################################\n");
-                printf("OBS:\n");
-                printf("Essa multiplicação é a mesma coisa que a soma de:\n");
-
-                for (i = 1; i < valor2; i++)
+                switch (tipo2)
                 {
 
-                    printf(" %.2f + ", valor1);
-                }
+                case 1:
 
-                printf("%.2f que é igual a %.2f\n", valor1, resultadoCal);
+                    // INterface do usuario da calculadora
+                    printf("SOMA:\n");
+                    printf("-->");
+                    scanf("%f", &valor1);
+                    printf("mais\n");
+                    printf("-->");
+                    scanf("%f", &valor2);
+                    printf("\n");
 
-                printf("Como a multiplicação é comutativa o oposto também é válido: \n");
+                    // realiza a operação de soma
+                    resultadoCal = valor1 + valor2;
 
-                for (i = 1; i < valor1; i++)
-                {
+                    printf(">>> %.2f + %.2f = %.2f\n", valor1, valor2, resultadoCal);
 
-                    printf(" %.2f + ", valor2);
-                }
+                    break;
 
-                printf("%.2f que é igual a %.2f\n", valor2, resultadoCal);
-                printf("#########################################################################################\n");
-                break;
+                case 2:
 
-            case 4:
+                    printf("SUBTRAÇÃO:\n");
+                    printf("-->");
+                    scanf("%f", &valor1);
+                    printf("menos\n");
+                    printf("-->");
+                    scanf("%f", &valor2);
+                    printf("\n");
 
-                printf("DIVISÃO:\n");
-                printf("-->");
-                scanf("%f", &valor1);
-                printf("dividido por\n");
-                scanf("%f", &valor2);
-                printf("\n");
+                    // Realiza a operação de subtração
+                    resultadoCal = valor1 - valor2;
 
-                // Realiza a operação de divisão
-                resultadoCal = valor1 / valor2;
+                    printf(">>> %.2f - %.2f = %.2f\n", valor1, valor2, resultadoCal);
+                    break;
 
-                valor1r = valor1;
-                valor2r = valor2;
-                resR = resultadoCal;
-                resto = valor1r % valor2r;
+                case 3:
 
-                printf("%4.2f/%4.2f=%4.2f\n", valor1, valor2, resultadoCal);
-                printf("Na divisão vc faz %dx%d\n", resR, valor2r);
+                    printf("MULTIPLICAÇÃO:\n");
+                    printf("-->");
+                    scanf("%f", &valor1);
+                    printf("vezes\n");
+                    printf("-->");
+                    scanf("%f", &valor2);
+                    printf("\n");
 
-                if (resto != 0)
-                {
+                    // Realiza a operação de multiplicação
+                    resultadoCal = valor1 * valor2;
 
-                    printf("mais o resto que é %d\n", resto);
-                }
+                    printf(">>> %.2f x %.2f = %.2f\n", valor1, valor2, resultadoCal);
+                    printf("\n");
+                    printf("#########################################################################################\n");
+                    printf("OBS:\n");
+                    printf("Essa multiplicação é a mesma coisa que a soma de:\n");
 
-                break;
-
-            case 5:
-
-                printf("PONTENCIAÇÃO:\n");
-                printf("OBS: INFORME UM NUMERO INTEIRO\n");
-                printf("-->");
-                scanf("%d", &valor1p);
-                printf("OBS: INFORME O EXPOENTE UM INTEIRO POSITIVO...\n");
-                printf("elevado a\n");
-                scanf("%d", &valor2p);
-                printf("\n");
-
-                // Realiza a operação de potencia
-                if (valor2p >= 0)
-                {
-                    for (i = 1; i <= valor2p; i++)
+                    for (i = 1; i < valor2; i++)
                     {
 
-                        pot *= valor1p;
+                        printf(" %.2f + ", valor1);
                     }
 
-                    printf("%d elevado a %d é igual a %d\n", valor1p, valor2p, pot);
-                }
-                else
-                {
-                    printf("ERRO\n");
-                    printf("Essa calculadora só realiza operações com expoente inteiro positivo...\n");
-                }
+                    printf("%.2f que é igual a %.2f\n", valor1, resultadoCal);
 
-                break;
+                    printf("Como a multiplicação é comutativa o oposto também é válido: \n");
 
-            default:
-                break;
-            }
-            printf("\n\n");
-            printf("...........................................................................................................................\n");
-            printf("%s deseja realizar nova operação:\nPra sim digite 1.\nDigite qualquer outro valor para sair.\n",nome);
-             printf("...........................................................................................................................\n");
-            scanf("%d",&opcao2);
-            system("clear");
+                    for (i = 1; i < valor1; i++)
+                    {
+
+                        printf(" %.2f + ", valor2);
+                    }
+
+                    printf("%.2f que é igual a %.2f\n", valor2, resultadoCal);
+                    printf("#########################################################################################\n");
+                    break;
+
+                case 4:
+
+                    printf("DIVISÃO:\n");
+                    printf("-->");
+                    scanf("%f", &valor1);
+                    printf("dividido por\n");
+                    printf("-->");
+                    scanf("%f", &valor2);
+                    printf("\n");
+
+                    // Realiza a operação de divisão
+                    resultadoCal = valor1 / valor2;
+
+
+                    printf(">>> %.2f : %.2f = %.2f\n", valor1, valor2, resultadoCal);
+                  
+                    break;
+
+                case 5:
+
+                    printf("PONTENCIAÇÃO:\n");
+                    printf("OBS: INFORME UM NUMERO INTEIRO\n");
+                    printf("-->");
+                    scanf("%d", &valor1p);
+                    printf("OBS: INFORME O EXPOENTE UM INTEIRO POSITIVO...\n");
+                    printf("elevado a\n");
+                    printf("-->");
+                    scanf("%d", &valor2p);
+                    printf("\n");
+
+                    // Realiza a operação de potencia
+                    if (valor2p >= 0)
+                    {
+                        for (i = 1; i <= valor2p; i++)
+                        {
+
+                            pot *= valor1p;
+                        }
+
+                        printf("%d elevado a %d é igual a %d\n", valor1p, valor2p, pot);
+                    }
+                    else
+                    {
+                        printf("ERRO\n");
+                        printf("Essa calculadora só realiza operações com expoente inteiro positivo...\n");
+                    }
+
+                    break;
+
+                default:
+                    break;
+                }
+                printf("\n\n");
+                printf("...........................................................................................................................\n");
+                printf("%s deseja realizar nova operação:\nPra sim digite 1.\nDigite qualquer outro valor para sair.\n", nome);
+                printf("...........................................................................................................................\n");
+                scanf("%d", &opcao2);
+                system("clear");
             }
         }
         else if (opcao == 3)
         {
 
-            /*IMPLEMENTAÇÃO DE UM MINI GAME...
-
-            No momento só foi adicionado a operação de multiplicação no mini game...
-            Futuramemte pretende adicionar para as outras operações também...
-
-            */
+            //IMPLEMENTAÇÃO DE UM MINI GAME...
 
             // variaveis do mini game
 
